64d974d638a45c38bf8772ea6af7c2d36964816c
PeaceZay <zay.smith007@gmail.com>
2022-12-30 14:32:02 -0600

    create, get and post

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const pool = require("./sql/connection");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.json({message: "hello universe!"})
 })
 
+// Get list of users
 app.get('/users', (req, res) => {
     pool.query("SELECT * FROM users", function (err, rows, fields) {
         // Connection is automatically released when query resolves
@@ -16,5 +19,22 @@ app.get('/users', (req, res) => {
     });
 });
 
+// Get user by id
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params;
+    pool.query(`SELECT * FROM users WHERE id = ${id}`, function (err, rows, fields) {
+        res.json(rows)
+    });
+});
 
+//Post -
+app.post('/users', (req, res) => {
+   console.log(req.body)
+    pool.query(
+        `INSERT INTO users (id, name, email, password) VALUES (?,?,?,?)`, 
+        [null, req.body.name, req.body.email, req.body.password],
+        function (err, row, fields) {
+        res.json(row)
+    });
+});
 app.listen(PORT, () => console.log(`Listening @ https://localhost:5000: ${PORT}`));
